66fcab1e232973128ae855fb471a45c8b114893c
Aleti-Bhavana <bhavanaaleti8897@gmail.com>
2025-11-07 03:37:06 +0000

    committed

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import {Component} from 'react'
+import {Switch, Route, Redirect} from 'react-router-dom'
+
+import Login from './components/Login'
+import ProtectedRoute from './components/ProtectedRoute'
+import Home from './components/Home'
+import VideoDetails from './components/VideoDetails'
+import Trending from './components/Trending'
+import Gaming from './components/Gaming'
+import SavedVideos from './components/SavedVideos'
+import NotFound from './components/NotFound'
+
+import {ContextProvider} from './context/Context'
+
+import './App.css'
+
+// Replace your code here
+class App extends Component {
+  state = {isSidebarOpen: false}
+
+  toggleSidebar = () => {
+    this.setState(prev => ({isSidebarOpen: !prev.isSidebarOpen}))
+  }
+
+  render() {
+    return (
+      <ContextProvider>
+        <Switch>
+          <Route exact path='/login' component={Login} />
+          <ProtectedRoute exact path='/' component={Home} />
+          <ProtectedRoute exact path='/trending' component={Trending} />
+          <ProtectedRoute exact path='/gaming' component={Gaming} />
+          <ProtectedRoute exact path='/saved-videos' component={SavedVideos} />
+          <ProtectedRoute exact path='/videos/:id' component={VideoDetails} />
+          <Route path='/not-found' component={NotFound} />
+          <Redirect to='/not-found' />
+        </Switch>
+      </ContextProvider>
+    )
+  }
+}
+
+export default App
